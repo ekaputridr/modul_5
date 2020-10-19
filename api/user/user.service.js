0775e6249dc18cbfc6e384c94d9d9920e4f56111
ekaputridr <60589563+ekaputridr@users.noreply.github.com>
2020-10-19 21:45:21 +0700

    Add files via upload

---
+++ api/user/user.service.js
@@ -0,0 +1,35 @@
+const db = require("../../config/connection");
+
+module.exports = {
+    serviceAddUser: (data, callBack) => {
+        db.query(
+        `insert into toko(firstName, lastName, gender, email, password, number)
+                values (?,?,?,?,?,?)`,
+        [
+            data.first_name,
+            data.last_name,
+            data.gender,
+            data.email,
+            data.password,
+            data.number
+        ],
+        (error, result) => {
+            if (error) {
+                return callBack(error);
+            } else {
+                return callBack(null, result);
+            }
+        }
+        );
+    },
+
+    serviceGetUsers: callBack => {
+        db.query(`select * from toko`, [], (err, results, fields) => {
+            if (err) {
+                return callBack(err);
+            } else {
+                return callBack(null, results);
+            }
+        });
+    },
+};
